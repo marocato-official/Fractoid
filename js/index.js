104984d1bd19b4caf436e0102db4444ff748b4ad
marocato-official <marocato.official@gmail.com>
2026-03-12 10:37:13 +0800

    attempt pwa install

--- js/index.js
+++ js/index.js
@@ -81,3 +81,22 @@ function util_MenuButtons(db) {
 		
 	}
 }
+
+let installPrompt = null;
+const installButton = document.querySelector("#install");
+
+window.addEventListener("beforeinstallprompt", (event) => {
+  event.preventDefault();
+  installPrompt = event;
+  installButton.removeAttribute("hidden");
+});
+
+installButton.addEventListener("click", async () => {
+  if (!installPrompt) {
+    return;
+  }
+  const result = await installPrompt.prompt();
+  console.log(`Install prompt was: ${result.outcome}`);
+  installPrompt = null;
+  installButton.setAttribute("hidden", "");
+});
